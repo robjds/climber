880b10abe2cb9d409b7dc87d1aac7dc81724ab32
robjds <rjds319@gmail.com>
2019-08-28 14:49:27 +0100

    fixed csv output formatting, tested sensor reading

--- sensor_logging.py
+++ sensor_logging.py
@@ -9,20 +9,26 @@ import os
 import csv
 import time
 
+def quitter():
+    tof.stop_ranging() # Stop ranging
+    ouputFile.close()
+    print 'bye ...'
+
+import atexit
+atexit.register(quitter)
+
 import VL53L1X
 
 from mpu9250 import mpu9250
-from time import sleep
 
 i = 0
 while os.path.exists("DataRun%s.csv" % i):
     i += 1
 
-headerRow = ['time', 'tof', 'aX', 'aY','aZ', 'gX', 'gY','gZ', 'mX', 'mY','mZ','temp']
-ouputFile = open("DataRun%s.csv" % i, "w")
-with ouputFile:
-   writer = csv.writer(ouputFile)
-   writer.writerows(headerRow)
+headerRow = ['time',' tof',' temp',' aX',' aY',' aZ',' gX',' gY',' gZ',' mX', ' mY',' mZ']
+ouputFile = open("DataRun%s.csv" % i, "a")
+writer = csv.writer(ouputFile)
+writer.writerow(headerRow)
 
 tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
 tof.open() # Initialise the i2c bus and configure the sensor
@@ -31,31 +37,21 @@ tof.start_ranging(1) # Start ranging, 1 = Short Range, 2 = Medium Range, 3 = Lon
 imu = mpu9250()
 
 print 'log started'
-startTime = time.process_time()
+startTime = time.time()
 
-try:
-	while True:
-
-        currentTime =  time.process_time() - startTime
-
-        distance_in_mm = tof.get_distance() # Grab the range in mm
-
-		gyr = imu.gyro
-		magn = imu.mag
-		temp = imu.temp
-        acce = imu.accel
-
-        outputData = ['{:.3}'.format(currentTime) ,
-                      '{:.3}'.format(distance_in_mm) ,
-                      '{:.3f},{:.3f},{:.3f}'.format(*acce) ,
-                      '{:.3f},{:.3f},{:.3f}'.format(*gyr) ,
-                      '{:.3f},{:.3f},{:.3f}'.format(*magn) ,
-                      '{:.3}'.format(temp)]
-
-        with ouputFile:
-            writer.writerows(outputData)
-
-except KeyboardInterrupt:
-    tof.stop_ranging() # Stop ranging
-    ouputFile.close();
-	print 'bye ...'
+while True:
+    currentTime = time.time() - startTime
+    distance_in_mm = tof.get_distance() # Grab the range in mm
+    gyr = imu.gyro
+    magn = imu.mag
+    temp = imu.temp
+    acce = imu.accel
+    # outputData = ['{:.3}'.format(currentTime),
+    #               ' {:04.3f}'.format(distance_in_mm/1000),
+    #               " {:.3f}, {:.3f}, {:.3f}".format(*acce),
+    #               ' {:.3f}, {:.3f}, {:.3f}'.format(*gyr),
+    #               ' {:.3f}, {:.3f}, {:.3f}'.format(*magn),
+    #               ' {:.3}'.format(temp)]
+    outputData = [currentTime,distance_in_mm,temp]
+    outputData.extend(list(acce) + list(gyr)+list(magn))
+    writer.writerow(outputData)
